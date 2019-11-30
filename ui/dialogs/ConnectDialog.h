9a40318ba7d8eba4f2260aeed6548da00ec876fc
shecks <shecks@gmail.com>
2019-11-30 23:36:35 +0000

    Initial work on server connection UI dialog.

---
+++ ui/dialogs/ConnectDialog.h
@@ -0,0 +1,34 @@
+/*
+ * File: ConnectDialog.h
+ * Created: 2019-11-30
+ *
+ * Copyright (c) shecks 2019 <shecks@gmail.com>
+ * All rights reserved.
+ *
+ * This file is part of %QT_PROJECT_NAME%.
+ *
+ * %QT_PROJECT_NAME% is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * %QT_PROJECT_NAME% is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with %QT_PROJECT_NAME%.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef CONNECTDIALOG_H
+#define CONNECTDIALOG_H
+
+
+class ConnectDialog : public QDialog
+{
+public:
+    ConnectDialog();
+};
+
+#endif // CONNECTDIALOG_H
